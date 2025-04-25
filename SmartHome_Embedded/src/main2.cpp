a72bc19e53e9fe7bf8b9303bbfe60af55a308671
Tung Nguyen <104656595+golthan@users.noreply.github.com>
2025-04-25 20:23:07 +0700

    Update main2.cpp

--- SmartHome_Embedded/src/main2.cpp
+++ SmartHome_Embedded/src/main2.cpp
@@ -3,7 +3,7 @@
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME280.h>
-#include "ds1307.hpp"  // dùng thư viện bạn upload
+#include "ds1307.hpp" 
 
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
